0d57bd58a13a5e9050b148124383d5b17e841734
Dmitriy <shalberkind@mail.ru>
2021-06-30 23:48:07 +0300

    Fetch posts from file on detailed post page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,21 +2,13 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 import cls from './post-content.module.css';
 
-const DUMMY_DATA = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting started with Nextjs',
-  image: 'getting-started-with-nextjs.png',
-  date: '2021-06-30',
-  content: '# This is first post',
-};
-
-const imagePath = `/images/posts/${DUMMY_DATA.slug}/${DUMMY_DATA.image}`;
-
 function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={cls.content}>
-      <PostHeader title={DUMMY_DATA.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_DATA.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
