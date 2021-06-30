c2d62f0be33d6cf21066eaa7e5ef451ab814483e
Dmitriy <shalberkind@mail.ru>
2021-06-30 23:48:42 +0300

    Fetch posts from file on all-posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,42 +1,18 @@
 import AllPosts from '../../components/posts/all-posts';
+import { getAllPosts } from '../../helpers/post-utils';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting started with Nextjs',
-    image: 'getting-started-with-nextjs.png',
-    excertp:
-      'NextJS is the React framework for production. It gives you the best developer experience with all the features you need for production: hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more. No config needed.',
-    date: '2021-06-30',
-  },
-  {
-    slug: 'getting-started-with-nextjs-2',
-    title: 'Getting started with Nextjs',
-    image: 'getting-started-with-nextjs.png',
-    excertp:
-      'NextJS is the React framework for production. It gives you the best developer experience with all the features you need for production: hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more. No config needed.',
-    date: '2021-06-30',
-  },
-  {
-    slug: 'getting-started-with-nextjs-3',
-    title: 'Getting started with Nextjs',
-    image: 'getting-started-with-nextjs.png',
-    excertp:
-      'NextJS is the React framework for production. It gives you the best developer experience with all the features you need for production: hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more. No config needed.',
-    date: '2021-06-30',
-  },
-  {
-    slug: 'getting-started-with-nextjs-4',
-    title: 'Getting started with Nextjs',
-    image: 'getting-started-with-nextjs.png',
-    excertp:
-      'NextJS is the React framework for production. It gives you the best developer experience with all the features you need for production: hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more. No config needed.',
-    date: '2021-06-30',
-  },
-];
+function AllPostsPage({ posts }) {
+  return <AllPosts posts={posts} />;
+}
 
-function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 }
 
 export default AllPostsPage;
