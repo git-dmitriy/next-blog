f65ec27f5829532710b077d8eb8b8a733311faa5
Dmitriy <shalberkind@mail.ru>
2021-07-03 23:58:09 +0300

    Sending data from the client to the api route

--- components/contact-page/contact-form.js
+++ components/contact-page/contact-form.js
@@ -1,23 +1,64 @@
+import { useState } from 'react';
 import cls from './contact-form.module.css';
 
 function ContactForm() {
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    message: '',
+  });
+
+  function onChangeHandler(event) {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  }
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify({ ...formData }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   return (
     <section className={cls.contact}>
       <h1>How can I help you?</h1>
-      <form className={cls.form}>
+      <form className={cls.form} onSubmit={sendMessageHandler}>
         <div className={cls.controls}>
           <div className={cls.control}>
             <label htmlFor='email'>Your email</label>
-            <input type='email' id='email' required />
+            <input
+              type='email'
+              id='email'
+              required
+              value={formData.enteredEmail}
+              onChange={onChangeHandler}
+            />
           </div>
           <div className={cls.control}>
             <label htmlFor='name'>Your name</label>
-            <input type='text' id='name' required />
+            <input
+              type='text'
+              id='name'
+              required
+              value={formData.enteredName}
+              onChange={onChangeHandler}
+            />
           </div>
         </div>
         <div className={cls.controls}>
           <label htmlFor='message'>Your message</label>
-          <textarea id='message' rows='5' />
+          <textarea
+            id='message'
+            rows='5'
+            required
+            value={formData.enteredMessage}
+            onChange={onChangeHandler}
+          />
         </div>
         <div className={cls.actions}>
           <button>Send Message</button>
