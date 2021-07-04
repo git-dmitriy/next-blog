807a0a85315ad2ed2112494313d8ad53125c2c87
Dmitriy <shalberkind@mail.ru>
2021-07-04 23:53:21 +0300

    Store messages with mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,8 @@
-function handler(req, res) {
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
+dotenv.config();
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -15,7 +19,28 @@ function handler(req, res) {
     }
 
     const newMessage = { email, name, message };
-    console.log(newMessage);
+
+    let client;
+    try {
+      client = await MongoClient.connect(process.env.API_KEY, {
+        useUnifiedTopology: true,
+      });
+    } catch (err) {
+      res.status(500).json({ message: 'Could not connect to database', err });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (err) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed' });
+      return;
+    }
+    client.close();
 
     res.status(201).json({
       message: 'Seccessfully stored message',
