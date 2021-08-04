a5479ec687e1b7a3cb8c373d69575b076b5dcd85
git-dmitriy <shalberkind@mail.ru>
2021-08-04 13:07:45 +0300

    Add head data

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import ContactForm from '../components/contact-page/contact-form';
 
 function ContactPage() {
-  return <ContactForm />;
+  return (
+    <>
+      <Head>
+        <title>Contact me</title>
+        <meta name='description' content='You can ask me a question' />
+      </Head>
+      <ContactForm />
+    </>
+  );
 }
 
 export default ContactPage;
